bad4604515f77790b950b741ee26efeb9f6b2ab1
Mauri-utn <56610750+Mauri-utn@users.noreply.github.com>
2019-10-17 09:45:47 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -46,7 +46,7 @@ void ponerMatEnCero(float mat[20][3]){
 
 }
 
-void FechaHora(){
+void FechaHora(){ /// MUESTRA FECHA Y HORA ACTUALES
 
 
     time_t tiempo;
@@ -56,6 +56,72 @@ void FechaHora(){
     ///FECHA ACTUAL tmPtr->tm_mday, tmPtr->tm_mon+1, 1900+tmPtr->tm_year
     cout << tmPtr->tm_mday <<"/"<<tmPtr->tm_mon+1<<"/"<<1900+tmPtr->tm_year<<endl;
     cout << tmPtr->tm_hour<<":"<<tmPtr->tm_min<<":"<<tmPtr->tm_sec<<endl;
+
+}
+
+
+class Fecha /// DEVUELVE FECHA Y HORA ACTUAL
+{
+private:
+    int anio,mes,dia,hora,minuto;
+    time_t tiempo;
+    char cad[80];
+    struct tm *tmPtr;
+public:
+    Fecha(int a,int b,int c,int d,int e)
+    {
+        anio=a;
+        mes=b;
+        dia=c;
+        hora=d;
+        minuto=e;
+    }
+    Fecha()
+    {
+        tiempo = time(NULL);
+        tmPtr = localtime(&tiempo);
+
+        anio=tmPtr->tm_year+1900;
+        mes=tmPtr->tm_mon+1;
+        dia=tmPtr->tm_mday;
+        hora=tmPtr->tm_hour;
+        minuto=tmPtr->tm_min;
+        /*printf("FECHA ACTUAL \n%d/%d/%d %d:%d:%d", tmPtr->tm_mday, tmPtr->tm_mon+1, 1900+tmPtr->tm_year, tmPtr->tm_hour, tmPtr->tm_min, tmPtr->tm_sec);
+        strftime(cad, 80, "%A %d de %B de %Y", tmPtr);
+        printf("\n\n%s", cad);*/
+    }
+    int mostar_anio()
+    {
+        return anio;
+    }
+    int mostar_mes()
+    {
+        return mes;
+    }
+    int mostar_dia()
+    {
+        return dia;
+    }
+    int mostar_hora()
+    {
+        return hora;
+    }
+    int mostar_minuto()
+    {
+        return minuto;
+    }
+};
+
+void mensajes(int opcion){ /// MENSAJES GENERALES
+
+switch(opcion){
+case 1:
+    cout << "ERROR AL ABRIR ARCHIVO"<< endl;
+    break;
+
+}
+
+
 
 }
 
