b2ed2e816c6d674234e883723508c134a5dbdd08
Mauri-utn <56610750+Mauri-utn@users.noreply.github.com>
2019-10-23 22:50:22 -0300

    Update menuHabitaciones.h

--- menuHabitaciones.h
+++ menuHabitaciones.h
@@ -1,11 +1,19 @@
+
 #ifndef MENUHABITACIONES_H_INCLUDED
+//#include "menuClientes.h"
+//#include "misFunciones.h"
+#include<cstdlib>
+#include<iostream>
+#include<stdio.h>
+#include<time.h>
+#include"string.h" /// POR QUE LO PONES ACA ?
 #define MENUHABITACIONES_H_INCLUDED
 
+const char *FILE_GASTOS = "habitaciones.dat";
+const char *FILE_HABITACIONES = "gastos.dat";
 
 
-#include "menuClientes.h"
-#include "misFunciones.h"
-
+using namespace std;
 int menuHabitaciones();
 
 /*class Archivos
@@ -25,14 +33,16 @@ public:
         }
      }
 };*/
-class FechaSistema{
+
+class FechaSistema
+{
 private:
     int anio,mes,dia,hora,minuto;
     time_t tiempo;
     char cad[80];
     struct tm *tmPtr;
 public:
-    Fecha(int a,int b,int c,int d,int e)
+    FechaSistema(int a,int b,int c,int d,int e)
     {
         anio=a;
         mes=b;
@@ -40,7 +50,7 @@ public:
         hora=d;
         minuto=e;
     }
-    Fecha()
+    FechaSistema()
     {
         tiempo = time(NULL);
         tmPtr = localtime(&tiempo);
@@ -74,25 +84,51 @@ public:
     {
         return minuto;
     }
+
 };
+bool comparar_fechas(char ini[11],char fin[11],int a,int m,int d)
+{
+    //if((a-1900>=strcat(ini[8],ini[9])-'0')&&(m>=strcat(ini[3],ini[4])-'0')&&(d>=strcat(ini[0],ini[1])-'0')&&(a-1900<=strcat(fin[8],fin[9])-'0')&&(m<=strcat(fin[3],fin[4])-'0')&&(d<=strcat(fin[0],fin[1])-'0'))
+    int di1,di2,mi1,mi2,ai1,ai2,df1,df2,mf1,mf2,af1,af2;
+    di1=ini[0]-'0';
+    di2=ini[1]-'0';
+    mi1=ini[3]-'0';
+    mi2=ini[4]-'0';
+    ai1=ini[8]-'0';
+    ai2=ini[9]-'0';
+    df1=fin[0]-'0';
+    df2=fin[1]-'0';
+    mf1=fin[3]-'0';
+    mf2=fin[4]-'0';
+    af1=fin[8]-'0';
+    af2=fin[9]-'0';
+    if((a-2000)>=((ai1*10)+ai2)&&(m>=((mi1*10)+mi2))&&(d>=(di1*10)+di2))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 class Gasto
 {
 private:
     int id;
     char habitacion[4],tipo;
     float importe;
-    string descripcion;
-    Fecha fec;
+    char descripcion[240];
+    FechaSistema fec;
 public:
-    void consulta_habitacion() {} ///Muestra los gastos correspondientes al ultimo cambio de estado
-    void consulta_habitacion_fecha() {} ///Muestra los gastos dentro de 2 fechas
-    void consulta_habitacion_tipo() {} ///Muestra todos los gastos de la habitacion y tipo seleccionado
-    void consulta_tipo() {} ///Muestra todos los gastos correspondientes al tipo seleccionado
-    float get_importe(){}///Devuelve el importe del gasto
-    float get_total() {} ///Devuelve el total del ultimo cambio de estado
-    void cargar() {} ///carga nuevo gasto
-    void anular() {} ///anula ultimo gasto
-    void modificar() {} ///modifica ultimo gasto
+    void consulta_habitacion(); ///Muestra los gastos correspondientes al ultimo cambio de estado
+    void consulta_habitacion_fecha(); ///Muestra los gastos dentro de 2 fechas
+    void consulta_habitacion_tipo(); ///Muestra todos los gastos de la habitacion y tipo seleccionado
+    void consulta_tipo(); ///Muestra todos los gastos correspondientes al tipo seleccionado
+    float get_importe();///Devuelve el importe del gasto
+    float get_total(); ///Devuelve el total del ultimo cambio de estado
+    void cargar();///carga nuevo gasto
+    void anular(); ///anula ultimo gasto
+    void modificar(); ///modifica ultimo gasto
 };
 class Habitacion
 {
@@ -103,41 +139,136 @@ protected:
 class Habitacion_ocupada:public Habitacion
 {
 private:
-    string nom_cliente,ape_cliente;
+    char nom_cliente[50],ape_cliente[50];
     char tarj_cliente[17],cod_tarjeta[5],venc_tarjeta[5],dni_cliente[9],serv_contratado[6],hora_limpieza[5],medio_pago;
     bool reserva;
     int tel_cliente;
     float cuenta,pago_adelantado;
-    Fecha fec_salida,fec_ingreso;
+    FechaSistema fec_salida,fec_ingreso;
     // Archivos arc;
 public:
-    void ocupar() {} ///Ocupa una habitacion y carga los datos
-    void desocupar() {} ///Libera una habitacion
-    void consultar() {} ///Consulta los datos de la habitacion seleccionada
-    void consulta_fecha(){}///Consulta los datos de la habitacion entre 2 fechas
+    void ocupar(); ///Ocupa una habitacion y carga los datos
+    void desocupar(); ///Libera una habitacion
+    void consultar(); ///Consulta los datos de la habitacion seleccionada
+    void consulta_fecha(); ///Consulta los datos de la habitacion entre 2 fechas
 };
 class Habitacion_mantenimiento:public Habitacion
 {
 private:
-    string descripcion;
+    char descripcion[240];
     float gasto_total;
     int dias_estimado;
 public:
-    void enviar_mantenimiento() {} ///Carga una habitacion al sistema de mantenimiento
-    void consultar() {} ///Consulta estado de mantenimiento de una habitacion
-    void consultar_general() {} ///consulta todas las habitaciones en mantenimiento
-    void alta() {} ///Da de alta una habitacion y la deja desocupada
+    void enviar_mantenimiento(); ///Carga una habitacion al sistema de mantenimiento
+    void consultar(); ///Consulta estado de mantenimiento de una habitacion
+    void consultar_general(); ///consulta todas las habitaciones en mantenimiento
+    void alta();///Da de alta una habitacion y la deja desocupada
 };
 class Habitacion_reserva:public Habitacion
 {
 private:
     float pago_adelantado;
-    Fecha fec_ingreso,fec_salida;
+    FechaSistema fec_ingreso,fec_salida;
 public:
-    void consulta() {} ///Consulta todas las habitaciones reservadas
-    void cancelar() {} ///Cancela una reserva
-    void cargar  () {} ///Carga una nueva reserva
+    void consulta(); ///Consulta todas las habitaciones reservadas
+    void cancelar(); ///Cancela una reserva
+    void cargar  (); ///Carga una nueva reserva
 };
+
+void Gasto::cargar()
+{
+    FILE *g;
+    g=fopen("gastos.dat","ab");
+    if(g==NULL)
+    {
+        cout<<"Fallo al abrir el archivo";
+        return;
+    }
+    fseek(g,0,2);
+    id=((ftell(g)/sizeof(*this))+1);
+    cout<<"Ingrese la habitacion que realizo el gasto: ";
+    cin>>habitacion;
+    cout<<"Ingrese el tipo de gasto: ";
+    cin>>tipo;
+    cout<<"Ingrese el importe del gasto: $";
+    cin>>importe;
+    cout<<"Ingrese la descripcion del importe: ";
+    cin.ignore();
+    cin.getline(descripcion,240);
+    fwrite(this,sizeof(*this),1,g);
+
+    fclose(g);
+
+}
+
+void Gasto::consulta_habitacion()
+{
+    FILE *g;
+    char cons[4];
+    g=fopen("gastos.dat","rb");
+    if(g==NULL)
+    {
+        cout<<"Fallo al abrir el archivo";
+        return;
+    }
+    cout<<"Ingrese la habitacion por la que quiere consultar: ";
+    cin>>cons;
+    while(fread(this,sizeof(*this),1,g)==1)
+    {
+        if(strcmp(habitacion,cons)==0)
+        {
+            cout<<id<<endl;
+            cout<<habitacion<<endl;
+            cout<<tipo<<endl;
+            cout<<importe<<endl;
+            cout<<descripcion<<endl<<endl;
+            cout<<fec.mostar_dia()<<"/"<<fec.mostar_mes()<<"/"<<fec.mostar_anio()<<"  "<<fec.mostar_hora()<<":"<<fec.mostar_minuto()<<endl;
+            cout<<"-------------------------------"<<endl;
+        }
+    }
+    fclose(g);
+}
+
+void Gasto::consulta_habitacion_fecha()
+{
+    FILE *g;
+    char cons[4],fecini[11],fecfin[11];
+    g=fopen("gastos.dat","rb");
+    if(g==NULL)
+    {
+        cout<<"Fallo al abrir el archivo";
+        return;
+    }
+    cout<<"Ingrese la habitacion por la que quiere consultar: ";
+    cin>>cons;
+    cout<<"Ingrese fecha de inicio de la busqueda";
+    cin>>fecini;
+    cout<<"Ingrese fecha de fin de la busqueda";
+    cin>>fecfin;
+    while(fread(this,sizeof(*this),1,g)==1)
+    {
+        if(strcmp(habitacion,cons)==0&&comparar_fechas(fecini,fecfin,fec.mostar_anio(),fec.mostar_mes(),fec.mostar_dia())==true)
+        {
+            cout<<id<<endl;
+            cout<<habitacion<<endl;
+            cout<<tipo<<endl;
+            cout<<importe<<endl;
+            cout<<descripcion<<endl<<endl;
+            cout<<fec.mostar_dia()<<"/"<<fec.mostar_mes()<<"/"<<fec.mostar_anio()<<"  "<<fec.mostar_hora()<<":"<<fec.mostar_minuto()<<endl;
+            cout<<"-------------------------------"<<endl;
+        }
+    }
+    fclose(g);
+}
+
+int menuHabitaciones(){
+
+cout << "En proceso ..."<< endl;
+pausa();
+return 0;
+}
+
+
 
 
 
